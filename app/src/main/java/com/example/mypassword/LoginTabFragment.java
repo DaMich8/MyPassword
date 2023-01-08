2340a3101afb5444cb759b6ed18ec891bb49572a
Darius Michalcevičius <michalcevicius.darius@gmail.com>
2023-01-09 01:28:39 +0200

    Login activity xml v0.0.0.3

---
+++ app/src/main/java/com/example/mypassword/LoginTabFragment.java
@@ -0,0 +1,47 @@
+package com.example.mypassword;
+
+import android.os.Bundle;
+
+import androidx.appcompat.widget.AppCompatButton;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
+public class LoginTabFragment extends Fragment {
+    EditText username, password;
+    TextView fPassword;
+    AppCompatButton login;
+    float alphaConstant=0;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        ViewGroup root = (ViewGroup) inflater.inflate(R.layout.fragment_login_tab, container, false);
+
+        username = root.findViewById(R.id.lUsername);
+        password = root.findViewById(R.id.lPassword);
+        fPassword = root.findViewById(R.id.lFPassword);
+        login = root.findViewById(R.id.lLoginButton);
+
+        username.setTranslationX(800);
+        password.setTranslationX(800);
+        fPassword.setTranslationX(800);
+        login.setTranslationX(800);
+
+        username.setAlpha(alphaConstant);
+        password.setAlpha(alphaConstant);
+        fPassword.setAlpha(alphaConstant);
+        login.setAlpha(alphaConstant);
+
+        username.animate().translationX(0).alpha(1).setDuration(800).setStartDelay(300).start();
+        password.animate().translationX(0).alpha(1).setDuration(800).setStartDelay(500).start();
+        fPassword.animate().translationX(0).alpha(1).setDuration(800).setStartDelay(500).start();
+        login.animate().translationX(0).alpha(1).setDuration(800).setStartDelay(700).start();
+
+        return root;
+    }
+}
--- app/src/main/java/com/example/mypassword/MainActivity.java
+++ app/src/main/java/com/example/mypassword/MainActivity.java
@@ -1,14 +1,72 @@
 package com.example.mypassword;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.view.Window;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
 
+    TabLayout tabLayout;
+    ViewPager viewPager;
+    FloatingActionButton facebook, google;
+    float alphaConstant = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide();
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
+
+        tabLayout = findViewById(R.id.lTabLayout);
+        viewPager = findViewById(R.id.lVIewPager);
+        facebook = findViewById(R.id.lFloatingButtonFacebook);
+        google = findViewById(R.id.lFloatingButtonGoogle);
+
+        tabLayout.addTab(tabLayout.newTab().setText("login"));
+        tabLayout.addTab(tabLayout.newTab().setText("Sign Up"));
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        final LoginAdapter loginAdapter = new LoginAdapter(getSupportFragmentManager(), this, tabLayout.getTabCount());
+        viewPager.setAdapter(loginAdapter);
+
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+
+        facebook.setTranslationY(300);
+        google.setTranslationY(300);
+        tabLayout.setTranslationY(300);
+
+        facebook.setAlpha(alphaConstant);
+        google.setAlpha(alphaConstant);
+        tabLayout.setAlpha(alphaConstant);
+        viewPager.setAlpha(alphaConstant);
+
+        facebook.animate().translationY(0).alpha(1).setDuration(1000).setStartDelay(400).start();
+        google.animate().translationY(0).alpha(1).setDuration(1000).setStartDelay(600).start();
+        tabLayout.animate().translationY(0).alpha(1).setDuration(1000).setStartDelay(800).start();
+        viewPager.animate().translationY(0).alpha(1).setDuration(1000).setStartDelay(1000).start();
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
     }
 }
